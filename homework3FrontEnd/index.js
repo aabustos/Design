4035ccb7d98075fe1729b11c5859762148f38dd7
aabustos <51560871+aabustos@users.noreply.github.com>
2020-03-04 23:37:55 -0600

    Add files via upload

---
+++ homework3FrontEnd/index.js
@@ -0,0 +1,10 @@
+console.log('This is going to be used to predict fuel rate');
+//let name = 'jonathan';
+//console.log(name);
+const name = 'Alexis';
+console.log(name);
+
+let colors = ['green', 'red', 'blue'];
+console.log(colors.length);
+colors[3] = 'orange';
+console.log(colors);
